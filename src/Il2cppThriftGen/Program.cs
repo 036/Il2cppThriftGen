1ed7bd28f7a1c87e77feb2df18826623c27b7ddd
Paradigm <rob@nulldev.uk>
2020-10-25 09:18:37 +0000

    Handle other data types

--- src/Il2cppThriftGen/Program.cs
+++ src/Il2cppThriftGen/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
 using System.Text;
 using Il2CppInspector;
 using Il2CppInspector.Reflection;
@@ -32,6 +31,7 @@ namespace Il2cppThriftGen
         };
         public static string MetadataFile = @"global-metadata.dat";
         public static string BinaryFile = @"libil2cpp.so";
+        private static StringBuilder thrift = new StringBuilder();
         static void Main(string[] args)
         {
             Console.WriteLine("Using Il2cppInspector to read il2cpp.so");
@@ -52,30 +52,68 @@ namespace Il2cppThriftGen
                 var name = message.CSharpName;
                 var fields = message.DeclaredFields.Where(f => f.CustomAttributes.Any(a => a.AttributeType.FullName == dataMember));
                 var props = message.DeclaredProperties.Where(p => p.CustomAttributes.Any(a => a.AttributeType.FullName == dataMember));
-
-                var thrift = new StringBuilder();
                 thrift.Append($"struct {name} {{\n");
 
                 // Output C# fields
                 foreach (var field in fields)
                 {
                     var pmAtt = field.CustomAttributes.First(a => a.AttributeType.FullName == dataMember);
-                    thriftTypes.TryGetValue(field.FieldType.FullName ?? string.Empty, out var thriftTypeName);
-                    thrift.Append($"  {thriftTypeName ?? field.FieldType.Name} {field.Name} ;\n");
+                    outputField(field.Name, field.FieldType, pmAtt);
                 }
 
                 // Output C# properties
                 foreach (var prop in props)
                 {
                     var pmAtt = prop.CustomAttributes.First(a => a.AttributeType.FullName == dataMember);
-                    thriftTypes.TryGetValue(prop.PropertyType.FullName ?? string.Empty, out var thriftTypeName);
-                    thrift.Append($"  {thriftTypeName ?? prop.PropertyType.Name} {prop.Name};\n");
+                    outputField(prop.Name, prop.PropertyType, pmAtt);
                 }
 
                 thrift.Append("}\n");
 
                 Console.WriteLine(thrift);
             }
+        }
+
+        private static void outputField(string name, TypeInfo type, CustomAttributeData pmAtt)
+        {
+            var isRepeated = type.IsArray;
+            var typeFullName = isRepeated ? type.ElementType.FullName : type.FullName ?? string.Empty;
+            var typeFriendlyName = isRepeated ? type.ElementType.Name : type.Name;
+
+            // Handle maps (IDictionary)
+            if (type.ImplementedInterfaces
+                .Any(i => i.FullName == "System.Collections.Generic.IDictionary`2"))
+            {
+
+                // This time we have two generic arguments to deal with - the key and the value
+                var keyFullName = type.GenericTypeArguments[0].FullName;
+                var valueFullName = type.GenericTypeArguments[1].FullName;
+
+                // We're going to have to deal with building this proto type name
+                // separately from the value types below
+                // We don't set isRepeated because it's implied by using a map type
+                thriftTypes.TryGetValue(keyFullName, out var keyFriendlyName);
+                thriftTypes.TryGetValue(valueFullName, out var valueFriendlyName);
+                typeFriendlyName = $"map<{keyFriendlyName ?? type.GenericTypeArguments[0].Name}" +
+                    $", {valueFriendlyName ?? type.GenericTypeArguments[1].Name}>";
+            }
+
+            if (thriftTypes.TryGetValue(typeFullName, out var protoTypeName))
+                typeFriendlyName = protoTypeName;
+
+            var annotatedName = typeFriendlyName;
+            if (isRepeated)
+                annotatedName = "repeated " + annotatedName;
+
+            //Handle lists
+            if (type.ImplementedInterfaces.Any(i => i.FullName == "System.Collections.Generic.IList`1"))
+            {
+                typeFullName = type.GenericTypeArguments[0].FullName;
+                typeFriendlyName = type.GenericTypeArguments[0].Name;
+                annotatedName = $"list<{typeFriendlyName}>";
+            }
+
+            thrift.Append($"  {annotatedName} {name}\n");
         }
     }
 
